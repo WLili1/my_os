5b6c03e45db12b69123aeff66f2a9db825122c84
kuli.wzq <kuli.wzq@alibaba-inc.com>
2021-12-12 15:36:33 +0800

    palloc

--- kernel/init.c
+++ kernel/init.c
@@ -2,9 +2,11 @@
 #include "print.h"
 #include "interrupt.h"
 #include "../device/timer.h"
+#include "memory.h"
 
 void init_all() {
     put_str("init_all\n");
     idt_init();     //初始化中断
     timer_init();   //初始化PIT
+    mem_init();	  // 初始化内存管理系统
 }

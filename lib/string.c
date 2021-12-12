492207aaa7470b2c67988e682fb7b1e9c7c12df8
kuli <775952911@qq.com>
2021-12-12 06:34:20 -0800

    fix something

--- lib/string.c
+++ lib/string.c
@@ -4,7 +4,7 @@
 
 /*将dst_起始的size个字节置为value*/
 void memset(void* dst_, uint8_t value, uint32_t size) {
-    ASSERT(des_ != NULL);
+    ASSERT(dst_ != NULL);
     uint8_t* dst = (uint8_t*) dst_;
     while(size-- > 0)
     {*dst++ = value;}

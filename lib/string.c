a082659c4c84bb71d37664c0023dc5a94d20d989
kuli.wzq <kuli.wzq@alibaba-inc.com>
2021-12-12 22:40:55 +0800

    *

--- lib/string.c
+++ lib/string.c
@@ -11,10 +11,10 @@ void memset(void* dst_, uint8_t value, uint32_t size) {
 }
 
 /*将src_起始的size个字节复制到dst_*/
-void memcpy(void* dst_, void* src_, uint32_t size) {
+void memcpy(void* dst_, const void* src_, uint32_t size) {
     ASSERT(dst_ != NULL && src_ != NULL);
     uint8_t* dst = dst_;
-    uint8_t src = src_;
+    const uint8_t* src = src_;
     while(size-- > 0)
         *dst++ = *src++;
 }

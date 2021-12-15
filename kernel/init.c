4bb88da29d4f0c39ef9c3eb1cf1b0904999300f8
kuli.wzq <kuli.wzq@alibaba-inc.com>
2021-12-15 23:20:49 +0800

    线程调度

--- kernel/init.c
+++ kernel/init.c
@@ -3,10 +3,12 @@
 #include "interrupt.h"
 #include "../device/timer.h"
 #include "memory.h"
+#include "thread.h"
 
 void init_all() {
     put_str("init_all\n");
-    idt_init();     //初始化中断
-    timer_init();   //初始化PIT
+    idt_init();    // 初始化中断
     mem_init();	  // 初始化内存管理系统
+    thread_init(); // 初始化线程相关结构
+    timer_init();  // 初始化PIT
 }

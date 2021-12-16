a4204f90c7a1be33d5249f7f5071387aee64d8b7
kuli.wzq <kuli.wzq@alibaba-inc.com>
2021-12-16 22:24:41 +0800

    sync

--- kernel/main.c
+++ kernel/main.c
@@ -2,6 +2,7 @@
 #include "init.h"
 #include "../thread/thread.h"
 #include "interrupt.h"
+#include "../device/console.h"
 
 void k_thread_a(void*);
 void k_thread_b(void*);
@@ -10,11 +11,11 @@ int main(void) {
     init_all();
 
     thread_start("k_thread_a", 31, k_thread_a, "argA ");
-    thread_start("k_thread_b", 8, k_thread_b, "argB ");
+    thread_start("k_thread_b", 8, k_thread_b, "ar_gB ");
 
-    intr_enable();	// 打开中断,使时钟中断起作用
+    intr_enable();
     while(1) {
-        put_str("Main ");
+        console_put_str("Main ");
     };
     return 0;
 }
@@ -24,7 +25,7 @@ void k_thread_a(void* arg) {
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
     char* para = arg;
     while(1) {
-        put_str(para);
+        console_put_str(para);
     }
 }
 
@@ -33,6 +34,6 @@ void k_thread_b(void* arg) {
 /* 用void*来通用表示参数,被调用的函数知道自己需要什么类型的参数,自己转换再用 */
     char* para = arg;
     while(1) {
-        put_str(para);
+        console_put_str(para);
     }
 }

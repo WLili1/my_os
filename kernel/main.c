973bf234d01a9920dea5e27e913d483c82cdad8b
kuli.wzq <kuli.wzq@alibaba-inc.com>
2022-01-09 11:11:20 +0800

    fix

--- kernel/main.c
+++ kernel/main.c
@@ -24,6 +24,8 @@ int main(void) {
     thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
     thread_start("k_thread_b", 31, k_thread_b, "I am thread_b");
 
+    sys_open("/file1", O_CREAT);
+
     uint32_t fd = sys_open("/file1", O_RDWR);
     printf("fd:%d\n", fd);
     sys_write(fd, "hello,world\n", 12);

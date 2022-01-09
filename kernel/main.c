0a3a52cab78e44e8e7116a87e4b72a0c8f9dfd47
kuli.wzq <kuli.wzq@alibaba-inc.com>
2022-01-09 11:18:58 +0800

    range dir

--- kernel/main.c
+++ kernel/main.c
@@ -24,38 +24,16 @@ int main(void) {
     thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
     thread_start("k_thread_b", 31, k_thread_b, "I am thread_b");
 
-    sys_open("/file1", O_CREAT);
-
-    uint32_t fd = sys_open("/file1", O_RDWR);
-    printf("fd:%d\n", fd);
-    sys_write(fd, "hello,world\n", 12);
-    sys_write(fd, "hello,world\n", 12);
-    sys_close(fd);
-
-//    *************************************
-    uint32_t fd2 = sys_open("/file1", O_RDWR);
-    char buf[64] = {0};
-
-    memset(buf, 0, 64);
-    int read_bytes = sys_read(fd2, buf, 24);
-    printf("4_ read %d bytes:\n%s", read_bytes, buf);
-
-    sys_close(fd2);
-
-
-//  *******************************************
-    printf("/dir1/subdir1 create %s!\n", sys_mkdir("/dir1/subdir1") == 0 ? "done" : "fail");
-    printf("/dir1 create %s!\n", sys_mkdir("/dir1") == 0 ? "done" : "fail");
-    printf("now, /dir1/subdir1 create %s!\n", sys_mkdir("/dir1/subdir1") == 0 ? "done" : "fail");
-    int fd1 = sys_open("/dir1/subdir1/file2", O_CREAT|O_RDWR);
-    if (fd1 != -1) {
-        printf("/dir1/subdir1/file2 create done!\n");
-        sys_write(fd1, "Catch me if you can!\n", 21);
-        sys_lseek(fd1, 0, SEEK_SET);
-        char buf1[32] = {0};
-        sys_read(fd1, buf1, 21);
-        printf("/dir1/subdir1/file2 says:\n%s", buf1);
-        sys_close(fd1);
+    struct dir* p_dir = sys_opendir("/dir1/subdir1");
+    if (p_dir) {
+        printf("/dir1/subdir1 open done!\n");
+        if (sys_closedir(p_dir) == 0) {
+            printf("/dir1/subdir1 close done!\n");
+        } else {
+            printf("/dir1/subdir1 close fail!\n");
+        }
+    } else {
+        printf("/dir1/subdir1 open fail!\n");
     }
    while(1);
    return 0;
